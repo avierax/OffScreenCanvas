efb8889c850fca3fd676753138721f30235f7564
Ariel Viera <ariel.viera@gmail.com>
2021-06-15 20:14:38 -0400

    Add project files.

---
+++ OffScreenCanvas/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OffScreenCanvas
+{
+    public partial class MainWindowViewModel : DependencyObject
+    {
+        public System.Windows.Controls.Image Image { get; set; }
+
+        public string Text {
+            get
+            {
+                return (string)GetValue(NameProperty);
+            }
+            set
+            {
+                SetValue(NameProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Text", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(null));
+
+        public MainWindowViewModel()
+        {
+            Text = "Hello World";
+        }
+
+    }
+}
